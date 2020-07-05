c317919548b3e632b99cc04260b16c38ce964ae7
knowledge <knowledgerelated96@gmail.com>
2020-07-05 18:54:05 +0530

    login,registration page template

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,13 @@
+
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+// import { ToastrService } from 'ngx-toastr';
+// import { AuthService } from 'src/app/services/auth.service';
+ import { Router } from '@angular/router';
+// import { UserService } from 'src/app/services/user.service';
+// import { UtililtyFunctions } from 'src/app/utils/utils';
+declare var $: any;
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +16,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  showPopup: boolean = false;
+  submitted: boolean = false;
+  errorMessage:string="";
+  calledFrom: string = "login";
+  resetPasswordToken:string = "";
+  userEmail = null;
+  showForgotPasswordtPopup = false;
+  verifyOtp = false;
+  showFirstTimePassPopup = false;
+
+  public loginInfo = new FormGroup({
+    usernameOrEmail: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required]),
+  });
+
+
+
+
+
+    constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  
+  get f() { return this.loginInfo.controls; }
+
+  // loginUser() {
+  //   this.submitted = true;
+  //   if (this.loginInfo.invalid) {
+  //     return;
+  //   }
+  //   this.errorMessage="";
+  //   let values = this.loginInfo.value;
+  //   this._authService.signIn(values).subscribe(data => {
+  //     if (data && data.data) {
+  //       console.log("loginUserResponseData..", data.data);
+  //       if (data.data.accessToken && data.data.accessToken != "" && data.data.accessToken != null) {
+  //         let datainput: any = {};
+  //         this._userService.getCurrentUserData(datainput).subscribe(data => {
+  //           if (data && data.data) {
+  //             this.router.navigate(['']);              
+  //             this.utilityservice.onLoginSuccessfully.next();
+  //           }
+  //         }, error => {
+  //         });
+  //       }
+  //     }
+  //   }, error => {
+  //     this.errorMessage = error.error.message;
+  //   });
+  // }
+
+  // getCurrentUserData() {
+  //   let datainput: any = {};
+  //   this._userService.getCurrentUserData(datainput).subscribe(data => {
+  //     if (data && data.data) {
+  //       this.router.navigate(['']);    
+  //       this.utilityservice.onLoginSuccessfully.next();
+  //     }
+  //   }, error => {
+
+  //   });
+  // }
+ 
+
+
+  
+
 }
+
