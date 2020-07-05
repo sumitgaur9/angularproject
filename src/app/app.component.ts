6e944017856e3cee69e33f09dafceab0d14096d6
SumitGaur92 <65018423+SumitGaur92@users.noreply.github.com>
2020-07-05 22:06:12 +0530

    header footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'newProject';
+
+  showHeaderFooter: boolean = true;
+
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit() {
+    
+  }
+
+  ngAfterViewInit(){
+    if (this.router.url == "/#/registration" || this.router.url == "/#/login") {
+      this.showHeaderFooter = false;
+    } else { this.showHeaderFooter = true; }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistrationComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
