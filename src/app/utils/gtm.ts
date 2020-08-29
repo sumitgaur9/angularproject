8c6dab8ae181c43f6b39fc5303831578e623bd48
QTPL0023 <sumit.gaur@qtsolv.com>
2020-08-29 10:23:00 +0530

    GTM code added

---
+++ src/app/utils/gtm.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GTMFunction {
+
+  constructor(private gtmService: GoogleTagManagerService) { }
+
+  pageview(page) {
+    console.log("this.gtmService", this.gtmService);
+    this.gtmService.pushTag({ event: 'page', pageName: page });
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  GTM_ID: 'GTM-M476F8N'
 };
 
 /*
