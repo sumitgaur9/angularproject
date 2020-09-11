dbd3aa1f89808697efb96b31b01810c9ae30b30c
QTPL0023 <sumit.gaur@qtsolv.com>
2020-09-11 21:59:02 +0530

    Captcha implementation in register form

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -19,6 +19,8 @@ export class RegistrationComponent implements OnInit {
 //    email: new FormControl("", [Validators.required, Validators.pattern(this.emailPattern)]),
 
   public submitted: boolean = false;
+  code;
+  isInvalidCaptcha:boolean = false;
 
   public userInfo = new FormGroup({
     email: new FormControl("", [Validators.required]),
@@ -32,6 +34,7 @@ export class RegistrationComponent implements OnInit {
   get f() { return this.userInfo.controls; }
 
   ngOnInit() {
+    this.createCaptcha();
   }
 
   constructor(private router: Router,private utilityservice:UtililtyFunctions, private _apiservice: APIService, private toastr: ToastrService) { }
@@ -47,7 +50,14 @@ export class RegistrationComponent implements OnInit {
       description: this.userInfo.value.description,
     }
     this.submitted = true;
+    this.isInvalidCaptcha = false;
+    if(!this.validateCaptcha()){
+      this.isInvalidCaptcha = true;
+      return;
+    }
     if (this.userInfo.invalid) {
+      this.isInvalidCaptcha = false;
+      this.createCaptcha();
       return;
     }
     let values = dataobj
@@ -67,6 +77,45 @@ export class RegistrationComponent implements OnInit {
     this.router.navigate(['/login']);
 
   }
+
+  createCaptcha() {
+    //clear the contents of captcha div first 
+    document.getElementById('captcha').innerHTML = "";
+    var charsArray =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%^&*";
+    var lengthOtp = 6;
+    var captcha = [];
+    for (var i = 0; i < lengthOtp; i++) {
+      //below code will not allow Repetition of Characters
+      var index = Math.floor(Math.random() * charsArray.length + 1); //get the next character from the array
+      if (captcha.indexOf(charsArray[index]) == -1)
+        captcha.push(charsArray[index]);
+      else i--;
+    }
+    var canv = document.createElement("canvas");
+    canv.id = "captcha";
+    canv.width = 100;
+    canv.height = 50;
+    var ctx = canv.getContext("2d");
+    ctx.font = "25px Georgia";
+    ctx.strokeText(captcha.join(""), 0, 30);
+    //storing captcha so that can validate you can save it somewhere else according to your specific requirements
+    this.code = captcha.join("");
+    document.getElementById("captcha").appendChild(canv); // adds the canvas to the body element
+  }
+
+  validateCaptcha() {
+    event.preventDefault();
+    debugger
+    let docelement = document.getElementById("cpatchaTextBox") as HTMLInputElement
+    if (docelement.value == this.code) {
+      return true;
+    }else{
+      this.createCaptcha();
+      return false;
+    }
+  }
+  
 
   getMessage(formcontrol: any, formControlName: any, fieldDisplayName: string) {
     // if(formControlName=='billingRate' && this.addForm.controls.clientContractStatus.value == 'fp')
