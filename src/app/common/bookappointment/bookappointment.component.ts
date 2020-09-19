592d4e4b2961a1e81cafcef5a50782745ac11ca0
QTPL0023 <sumit.gaur@qtsolv.com>
2020-09-19 21:33:42 +0530

    bookappointment min max date changed

--- src/app/common/bookappointment/bookappointment.component.ts
+++ src/app/common/bookappointment/bookappointment.component.ts
@@ -67,8 +67,8 @@ export class BookappointmentComponent implements OnInit {
     format: "DD/MM/YYYY",
     monthFormat: "MMMM, YYYY",
     firstDayOfWeek: "mo",
-    min: "01/07/2020",
-    max: "30/08/2020"
+    min: "01/09/2020",
+    max: "30/11/2020"
   };
   textareaValue: string = '';
 
