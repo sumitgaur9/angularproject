55e73aef91d932f634a299883a8af5ab542008cf
QTPL0023 <sumit.gaur@qtsolv.com>
2020-10-04 13:02:25 +0530

    cookies sendTokenToServer() in service worker

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -12,3 +12,21 @@ firebase.initializeApp({
     measurementId: "G-LXVLR8JQ89"
 });
 const messaging = firebase.messaging();
+
+//for testing message to specific user
+messaging.getToken().then((currentToken) => {
+    if (currentToken) {
+      sendTokenToServer(currentToken);
+      updateUIForPushEnabled(currentToken);
+    } else {
+      // Show permission request.
+      console.log('No Instance ID token available. Request permission to generate one.');
+      // Show permission UI.
+      updateUIForPushPermissionRequired();
+      setTokenSentToServer(false);
+    }
+  }).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    showToken('Error retrieving Instance ID token. ', err);
+    setTokenSentToServer(false);
+  });
