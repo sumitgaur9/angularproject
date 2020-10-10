f36e514aabd142399fcecd82c2539bd37272a7fc
knowledgerelated <knowledgerelated96@gmail.com>
2020-10-10 22:07:35 +0530

    browse button style change

--- src/app/common/medicineprofile/medicineprofile.component.ts
+++ src/app/common/medicineprofile/medicineprofile.component.ts
@@ -31,8 +31,6 @@ export class MedicineprofileComponent implements OnInit {
   errorMessage = '';
   emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
 
-  // public medicineDataArray = [{ "name": "Cipla" }, { "name": "Crosin" },{ "name": "Jufex Fort" }];
-  public companyNameDataArray = [{ "name": "Aimil" }, { "name": "cipla" }, {"name":"GSK"}];
   public companyArrayData:any=[];
   public medicineListDataArray:any=[];
 
